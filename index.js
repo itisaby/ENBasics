66af903c776cf9f08d9563d6d5a2c97e2ef6225a
itisaby <arnabbibhuti01@gmail.com>
2022-02-28 00:08:08 +0530

    Form Validator

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const { body, validationResult } = require('express-validator');
 const port = 8080;
 app.use('/static',express.static('public'))
 app.set('view engine', 'ejs');
